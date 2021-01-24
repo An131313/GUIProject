e6ac7300ce78fa770e4b53054fa3cfb180cf3fa6
An131313 <71447680+An131313@users.noreply.github.com>
2021-01-24 22:40:01 +0200

    Step 7 - final

--- Colors.java
+++ Colors.java
@@ -102,7 +102,7 @@ public class Colors extends JFrame {
 	public Colors() {
 		setTitle("Colors");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 667, 695);
+		setBounds(100, 100, 656, 695);
 		contentPane = new JPanel();
 		contentPane.setBackground(Color.WHITE);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -114,7 +114,7 @@ public class Colors extends JFrame {
 		sliderRed.setForeground(Color.BLACK);
 		sliderRed.setValue(0);
 		sliderRed.setMaximum(255);
-		sliderRed.setBounds(39, 69, 200, 26);
+		sliderRed.setBounds(39, 69, 252, 26);
 		contentPane.add(sliderRed);
 		
 		sliderGreen = new JSlider();
@@ -122,7 +122,7 @@ public class Colors extends JFrame {
 		sliderGreen.setForeground(Color.BLACK);
 		sliderGreen.setValue(0);
 		sliderGreen.setMaximum(255);
-		sliderGreen.setBounds(39, 132, 200, 26);
+		sliderGreen.setBounds(39, 132, 252, 26);
 		contentPane.add(sliderGreen);
 		
 		sliderBlue = new JSlider();
@@ -130,57 +130,64 @@ public class Colors extends JFrame {
 		sliderBlue.setForeground(Color.BLACK);
 		sliderBlue.setValue(0);
 		sliderBlue.setMaximum(255);
-		sliderBlue.setBounds(39, 199, 200, 26);
+		sliderBlue.setBounds(39, 199, 252, 26);
 		contentPane.add(sliderBlue);		
 	    
 		redValue = new JTextField();
-		redValue.setBounds(251, 69, 78, 22);
+		redValue.setFont(new Font("Century751 No2 BT", Font.PLAIN, 14));
+		redValue.setBounds(303, 69, 35, 22);
 		contentPane.add(redValue);
 		redValue.setColumns(10);
 		redValue.setEditable(false);
 		
 		greenValue = new JTextField();
+		greenValue.setFont(new Font("Century751 No2 BT", Font.PLAIN, 14));
 		greenValue.setColumns(10);
-		greenValue.setBounds(251, 132, 78, 22);
+		greenValue.setBounds(303, 132, 35, 22);
 		contentPane.add(greenValue);
 		greenValue.setEditable(false);
 		
 		blueValue = new JTextField();
+		blueValue.setFont(new Font("Century751 No2 BT", Font.PLAIN, 14));
 		blueValue.setColumns(10);
-		blueValue.setBounds(251, 203, 78, 22);
+		blueValue.setBounds(303, 201, 35, 22);
 		contentPane.add(blueValue);
 		blueValue.setEditable(false);
 		
 		lblR = new JLabel("R");
-		lblR.setFont(new Font("Tahoma", Font.BOLD, 17));
+		lblR.setForeground(Color.BLACK);
+		lblR.setFont(new Font("Century751 No2 BT", Font.BOLD, 22));
 		lblR.setBounds(12, 69, 24, 26);
 		contentPane.add(lblR);
 		
 		lblG = new JLabel("G");
-		lblG.setFont(new Font("Tahoma", Font.BOLD, 17));
+		lblG.setForeground(Color.BLACK);
+		lblG.setFont(new Font("Century751 No2 BT", Font.BOLD, 22));
 		lblG.setBounds(12, 132, 24, 26);
 		contentPane.add(lblG);
 		
 		lblB = new JLabel("B");
-		lblB.setFont(new Font("Tahoma", Font.BOLD, 17));
+		lblB.setForeground(Color.BLACK);
+		lblB.setFont(new Font("Century751 No2 BT", Font.BOLD, 22));
 		lblB.setBounds(12, 199, 24, 26);
 		contentPane.add(lblB);
 		
 		colorRGB = new JTextArea(); //the color will be displayed here
-		colorRGB.setBounds(352, 13, 284, 261);
+		colorRGB.setBounds(350, 14, 276, 261);
 		contentPane.add(colorRGB);
 		colorRGB.setEditable(false);
 		
 		colorTag = new JTextField(); //the hexadecimal number of the color will be displayed here
+		colorTag.setFont(new Font("Century751 No2 BT", Font.PLAIN, 14));
 		colorTag.setText("000000");
 		colorTag.setColumns(10);
-		colorTag.setBounds(558, 276, 78, 22);
+		colorTag.setBounds(570, 280, 56, 22);
 		contentPane.add(colorTag);
 		colorTag.setEditable(false);
 		
 		JLabel label = new JLabel("#");
-		label.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		label.setBounds(540, 272, 16, 26);
+		label.setFont(new Font("Century751 No2 BT", Font.PLAIN, 17));
+		label.setBounds(552, 276, 16, 26);
 		contentPane.add(label);
 			
 		sliderRed.addChangeListener(new ChangeListener() {
@@ -219,86 +226,95 @@ public class Colors extends JFrame {
         firstSavedColor = new JTextArea();
         firstSavedColor.setEditable(false);
         firstSavedColor.setBackground(Color.BLACK);
-        firstSavedColor.setBounds(189, 366, 109, 107);
+        firstSavedColor.setBounds(177, 367, 109, 107);
         contentPane.add(firstSavedColor);
         
         secondSavedColor = new JTextArea();
         secondSavedColor.setEditable(false);
         secondSavedColor.setBackground(Color.BLACK);
-        secondSavedColor.setBounds(302, 366, 109, 107);
+        secondSavedColor.setBounds(290, 367, 109, 107);
         contentPane.add(secondSavedColor);
         
         thirdSavedColor = new JTextArea();
         thirdSavedColor.setEditable(false);
         thirdSavedColor.setBackground(Color.BLACK);
-        thirdSavedColor.setBounds(415, 366, 109, 107);
+        thirdSavedColor.setBounds(403, 367, 109, 107);
         contentPane.add(thirdSavedColor);
         
         fourthSavedColor = new JTextArea();
         fourthSavedColor.setEditable(false);
         fourthSavedColor.setBackground(Color.BLACK);
-        fourthSavedColor.setBounds(529, 366, 109, 107);
+        fourthSavedColor.setBounds(517, 367, 109, 107);
         contentPane.add(fourthSavedColor);
         
         fifthSavedColor = new JTextArea();
         fifthSavedColor.setEditable(false);
         fifthSavedColor.setBackground(Color.BLACK);
-        fifthSavedColor.setBounds(189, 476, 109, 107);
+        fifthSavedColor.setBounds(177, 477, 109, 107);
         contentPane.add(fifthSavedColor);
         
         sixthSavedColor = new JTextArea();
         sixthSavedColor.setEditable(false);
         sixthSavedColor.setBackground(Color.BLACK);
-        sixthSavedColor.setBounds(302, 476, 109, 107);
+        sixthSavedColor.setBounds(290, 477, 109, 107);
         contentPane.add(sixthSavedColor);
         
         seventhSavedColor = new JTextArea();
         seventhSavedColor.setEditable(false);
         seventhSavedColor.setBackground(Color.BLACK);
-        seventhSavedColor.setBounds(415, 476, 109, 107);
+        seventhSavedColor.setBounds(403, 477, 109, 107);
         contentPane.add(seventhSavedColor);
         
         eigthSavedColor = new JTextArea();
         eigthSavedColor.setEditable(false);
         eigthSavedColor.setBackground(Color.BLACK);
-        eigthSavedColor.setBounds(528, 476, 109, 107);
+        eigthSavedColor.setBounds(516, 477, 109, 107);
         contentPane.add(eigthSavedColor);
         
         //the hexadecimal number of saved colors will be displayed in those 
         firstSavedColorLabel = new JLabel(" ");
-        firstSavedColorLabel.setBounds(215, 344, 56, 16);
+        firstSavedColorLabel.setFont(new Font("Century751 No2 BT", Font.PLAIN, 14));
+        firstSavedColorLabel.setBounds(208, 345, 56, 16);
         contentPane.add(firstSavedColorLabel);
         
         secondSavedColorLabel = new JLabel(" ");
-        secondSavedColorLabel.setBounds(320, 344, 56, 16);
+        secondSavedColorLabel.setFont(new Font("Century751 No2 BT", Font.PLAIN, 14));
+        secondSavedColorLabel.setBounds(320, 345, 56, 16);
         contentPane.add(secondSavedColorLabel);
         
         thirdSavedColorLabel = new JLabel(" ");
-        thirdSavedColorLabel.setBounds(436, 344, 56, 16);
+        thirdSavedColorLabel.setFont(new Font("Century751 No2 BT", Font.PLAIN, 14));
+        thirdSavedColorLabel.setBounds(434, 345, 56, 16);
         contentPane.add(thirdSavedColorLabel);
         
         fourthSavedColorLabel = new JLabel(" ");
-        fourthSavedColorLabel.setBounds(555, 344, 56, 16);
+        fourthSavedColorLabel.setFont(new Font("Century751 No2 BT", Font.PLAIN, 14));
+        fourthSavedColorLabel.setBounds(548, 345, 56, 16);
         contentPane.add(fourthSavedColorLabel);
         
         fifthSavedColorLabel = new JLabel(" ");
-        fifthSavedColorLabel.setBounds(215, 589, 56, 16);
+        fifthSavedColorLabel.setFont(new Font("Century751 No2 BT", Font.PLAIN, 14));
+        fifthSavedColorLabel.setBounds(210, 587, 56, 16);
         contentPane.add(fifthSavedColorLabel);
         
         sixthSavedColorLabel = new JLabel(" ");
-        sixthSavedColorLabel.setBounds(327, 589, 56, 16);
+        sixthSavedColorLabel.setFont(new Font("Century751 No2 BT", Font.PLAIN, 14));
+        sixthSavedColorLabel.setBounds(322, 587, 56, 16);
         contentPane.add(sixthSavedColorLabel);
         
         seventhSavedColorLabel = new JLabel(" ");
-        seventhSavedColorLabel.setBounds(441, 589, 56, 16);
+        seventhSavedColorLabel.setFont(new Font("Century751 No2 BT", Font.PLAIN, 14));
+        seventhSavedColorLabel.setBounds(436, 587, 56, 16);
         contentPane.add(seventhSavedColorLabel);
         
         eigthSavedColorLabel = new JLabel(" ");
-        eigthSavedColorLabel.setBounds(553, 589, 56, 16);
+        eigthSavedColorLabel.setFont(new Font("Century751 No2 BT", Font.PLAIN, 14));
+        eigthSavedColorLabel.setBounds(548, 587, 56, 16);
         contentPane.add(eigthSavedColorLabel);
         
         //the saved colors in the boxes can be changed multiple times
         JButton printFirstColorButton = new JButton("Print Color 1");
+        printFirstColorButton.setFont(new Font("Microsoft PhagsPa", Font.PLAIN, 13));
         printFirstColorButton.setBackground(Color.BLACK);
         printFirstColorButton.setForeground(Color.WHITE);
         printFirstColorButton.addActionListener(new ActionListener() {
@@ -308,10 +324,11 @@ public class Colors extends JFrame {
         	}
         });
         
-        printFirstColorButton.setBounds(189, 314, 108, 25);
+        printFirstColorButton.setBounds(177, 315, 108, 25);
         contentPane.add(printFirstColorButton);
         
         JButton printSecondColorButton = new JButton("Print Color 2");
+        printSecondColorButton.setFont(new Font("Microsoft PhagsPa", Font.PLAIN, 13));
         printSecondColorButton.setBackground(Color.BLACK);
         printSecondColorButton.setForeground(Color.WHITE);
         printSecondColorButton.addActionListener(new ActionListener() {
@@ -320,10 +337,11 @@ public class Colors extends JFrame {
         		secondSavedColorLabel.setText(colorTag.getText());
         	}
         });
-        printSecondColorButton.setBounds(302, 314, 108, 25);
+        printSecondColorButton.setBounds(290, 315, 108, 25);
         contentPane.add(printSecondColorButton);
         
         JButton printThirdColorButton = new JButton("Print Color 3");
+        printThirdColorButton.setFont(new Font("Microsoft PhagsPa", Font.PLAIN, 13));
         printThirdColorButton.setBackground(Color.BLACK);
         printThirdColorButton.setForeground(Color.WHITE);
         printThirdColorButton.addActionListener(new ActionListener() {
@@ -332,10 +350,11 @@ public class Colors extends JFrame {
         		thirdSavedColorLabel.setText(colorTag.getText());
         	}
         });
-        printThirdColorButton.setBounds(415, 314, 108, 25);
+        printThirdColorButton.setBounds(403, 315, 108, 25);
         contentPane.add(printThirdColorButton);
         
         JButton printFourthColorButton = new JButton("Print Color 4");
+        printFourthColorButton.setFont(new Font("Microsoft PhagsPa", Font.PLAIN, 13));
         printFourthColorButton.setBackground(Color.BLACK);
         printFourthColorButton.setForeground(Color.WHITE);
         printFourthColorButton.addActionListener(new ActionListener() {
@@ -344,10 +363,11 @@ public class Colors extends JFrame {
         		fourthSavedColorLabel.setText(colorTag.getText());
         	}
         });
-        printFourthColorButton.setBounds(530, 314, 108, 25);
+        printFourthColorButton.setBounds(518, 315, 108, 25);
         contentPane.add(printFourthColorButton);
         
         printFifthColorButton = new JButton("Print Color 5");
+        printFifthColorButton.setFont(new Font("Microsoft PhagsPa", Font.PLAIN, 13));
         printFifthColorButton.setBackground(Color.BLACK);
         printFifthColorButton.setForeground(Color.WHITE);
         printFifthColorButton.addActionListener(new ActionListener() {
@@ -356,10 +376,11 @@ public class Colors extends JFrame {
         		fifthSavedColorLabel.setText(colorTag.getText());
         	}
         });
-        printFifthColorButton.setBounds(190, 609, 108, 25);
+        printFifthColorButton.setBounds(178, 610, 108, 25);
         contentPane.add(printFifthColorButton);
         
         printSixthColorButton = new JButton("Print Color 6");
+        printSixthColorButton.setFont(new Font("Microsoft PhagsPa", Font.PLAIN, 13));
         printSixthColorButton.setBackground(Color.BLACK);
         printSixthColorButton.setForeground(Color.WHITE);
         printSixthColorButton.addActionListener(new ActionListener() {
@@ -368,10 +389,11 @@ public class Colors extends JFrame {
         		sixthSavedColorLabel.setText(colorTag.getText());
         	}
         });
-        printSixthColorButton.setBounds(302, 609, 108, 25);
+        printSixthColorButton.setBounds(290, 610, 108, 25);
         contentPane.add(printSixthColorButton);
         
         printSeventhColorButton = new JButton("Print Color 7");
+        printSeventhColorButton.setFont(new Font("Microsoft PhagsPa", Font.PLAIN, 13));
         printSeventhColorButton.setBackground(Color.BLACK);
         printSeventhColorButton.setForeground(Color.WHITE);
         printSeventhColorButton.addActionListener(new ActionListener() {
@@ -380,10 +402,11 @@ public class Colors extends JFrame {
         		seventhSavedColorLabel.setText(colorTag.getText());
         	}
         });
-        printSeventhColorButton.setBounds(416, 609, 108, 25);
+        printSeventhColorButton.setBounds(404, 610, 108, 25);
         contentPane.add(printSeventhColorButton);
         
         printEigthSavedColorButton = new JButton("Print Color 8");
+        printEigthSavedColorButton.setFont(new Font("Microsoft PhagsPa", Font.PLAIN, 13));
         printEigthSavedColorButton.setBackground(Color.BLACK);
         printEigthSavedColorButton.setForeground(Color.WHITE);
         printEigthSavedColorButton.addActionListener(new ActionListener() {
@@ -392,59 +415,60 @@ public class Colors extends JFrame {
         		eigthSavedColorLabel.setText(colorTag.getText());
         	}
         });
-        printEigthSavedColorButton.setBounds(528, 609, 108, 25);
+        printEigthSavedColorButton.setBounds(516, 610, 108, 25);
         contentPane.add(printEigthSavedColorButton);
         
         JButton printButton = new JButton("Print color tag");
+        printButton.setFont(new Font("Microsoft PhagsPa", Font.PLAIN, 13));
         printButton.setForeground(Color.WHITE);
         printButton.setBackground(Color.BLACK);
-        printButton.setBounds(26, 311, 115, 25);
+        printButton.setBounds(12, 314, 148, 25);
         contentPane.add(printButton);
 
        //the hexadecimal numbers of colors (in the given color) will be displayed in those when printButton is clicked 
         coloredTag1 = new JLabel("");
-        coloredTag1.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 15));
-        coloredTag1.setBounds(32, 352, 105, 26);
+        coloredTag1.setFont(new Font("Century751 No2 BT", Font.PLAIN, 17));
+        coloredTag1.setBounds(59, 358, 74, 26);
         contentPane.add(coloredTag1);
         
         coloredTag2 = new JLabel("");
-        coloredTag2.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 15));
-        coloredTag2.setBounds(32, 381, 105, 26);
+        coloredTag2.setFont(new Font("Century751 No2 BT", Font.PLAIN, 17));
+        coloredTag2.setBounds(59, 387, 74, 26);
         contentPane.add(coloredTag2);
         
         coloredTag3 = new JLabel("");
-        coloredTag3.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 15));
-        coloredTag3.setBounds(32, 409, 105, 26);
+        coloredTag3.setFont(new Font("Century751 No2 BT", Font.PLAIN, 17));
+        coloredTag3.setBounds(59, 415, 74, 26);
         contentPane.add(coloredTag3);
         
         coloredTag4 = new JLabel("");
-        coloredTag4.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 15));
-        coloredTag4.setBounds(32, 439, 105, 26);
+        coloredTag4.setFont(new Font("Century751 No2 BT", Font.PLAIN, 17));
+        coloredTag4.setBounds(59, 445, 74, 26);
         contentPane.add(coloredTag4);
         
         coloredTag5 = new JLabel("");
-        coloredTag5.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 15));
-        coloredTag5.setBounds(32, 468, 105, 26);
+        coloredTag5.setFont(new Font("Century751 No2 BT", Font.PLAIN, 17));
+        coloredTag5.setBounds(59, 474, 74, 26);
         contentPane.add(coloredTag5);
         
         coloredTag6 = new JLabel("");
-        coloredTag6.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 15));
-        coloredTag6.setBounds(32, 496, 105, 26);
+        coloredTag6.setFont(new Font("Century751 No2 BT", Font.PLAIN, 17));
+        coloredTag6.setBounds(59, 502, 74, 26);
         contentPane.add(coloredTag6);
         
         coloredTag7 = new JLabel("");
-        coloredTag7.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 15));
-        coloredTag7.setBounds(32, 528, 105, 26);
+        coloredTag7.setFont(new Font("Century751 No2 BT", Font.PLAIN, 17));
+        coloredTag7.setBounds(59, 534, 74, 26);
         contentPane.add(coloredTag7);
         
         coloredTag8 = new JLabel("");
-        coloredTag8.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 15));
-        coloredTag8.setBounds(32, 557, 105, 26);
+        coloredTag8.setFont(new Font("Century751 No2 BT", Font.PLAIN, 17));
+        coloredTag8.setBounds(59, 563, 74, 26);
         contentPane.add(coloredTag8);
         
         coloredTag9 = new JLabel("");
-        coloredTag9.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 15));
-        coloredTag9.setBounds(32, 585, 105, 26);
+        coloredTag9.setFont(new Font("Century751 No2 BT", Font.PLAIN, 17));
+        coloredTag9.setBounds(59, 591, 74, 26);
         contentPane.add(coloredTag9);
         
         //only for the print area to be distinct from the background
@@ -452,7 +476,7 @@ public class Colors extends JFrame {
         textField.setBackground(Color.WHITE);
         textField.setForeground(Color.WHITE);
         textField.setEditable(false);
-        textField.setBounds(12, 341, 148, 284);
+        textField.setBounds(12, 350, 148, 277);
         contentPane.add(textField);
         textField.setColumns(10);
         
